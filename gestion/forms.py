0960585c0b7400839009cf25a1ee598267eb2c89
Yeremy-Molina-Boundpay <boundpay.yeremymolina@gmail.com>
2024-12-10 02:12:34 -0300

    Creacion de productos(No se si sirve en el contexto)

--- gestion/forms.py
+++ gestion/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import modelformset_factory
-from .models import Pedido, DetallePedido
+from .models import Pedido, DetallePedido, Producto
 
 class PedidoForm(forms.ModelForm):
     class Meta:
@@ -28,3 +28,25 @@ DetallePedidoFormSet = modelformset_factory(
     extra=1,  
     can_delete=True  # Permitir eliminación de registros
 )
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'categoria', 'precio_unitario', 'cantidad_en_stock']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'precio_unitario': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cantidad_en_stock': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+
+class ActualizarStockForm(forms.Form):
+    cantidad = forms.IntegerField(min_value=1, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    es_entrada = forms.BooleanField(
+        required=False,
+        initial=True,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        label="¿Es entrada de stock?"
+    )
--- gestion/urls.py
+++ gestion/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('<int:id>/editar/', views.editar_pedido, name='editar_pedido'),
     path('<int:id>/eliminar/', views.eliminar_pedido, name='eliminar_pedido'),
     path('eliminar_producto/<int:detalle_id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/crear/', views.crear_producto, name='crear_producto'),
+    path('productos/editar/<int:pk>/', views.editar_producto, name='editar_producto'),
+    path('productos/eliminar/<int:pk>/', views.eliminar_producto1, name='eliminar_producto1'),
+    path('productos/actualizar-stock/<int:pk>/', views.actualizar_stock, name='actualizar_stock'),
 
 ]
